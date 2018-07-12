058ce43f00a84bd5a69c056eba306b4f1c1d8f83
snarksliveshere <snarksliveshere@gmail.com>
2018-07-12 22:29:58 +0300

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
- <h1>clean project</h1>
+    <v-app>
+        <v-navigation-drawer app temporary></v-navigation-drawer>
+        <v-toolbar app dark color="primary">
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>VUESPA</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat>Link One</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
-
 <script>
-export default {}
+    export default {}
 </script>
