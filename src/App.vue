9fa569a797e53294e36e1ec0d07e4d9cb75b5099
snarksliveshere <snarksliveshere@gmail.com>
2018-07-13 21:52:33 +0300

    add navigation & routes & blank pages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
     <v-app>
-        <v-navigation-drawer app temporary></v-navigation-drawer>
+        <v-navigation-drawer app temporary
+                             v-model="drawer"
+        >
+            <v-list>
+                <v-list-tile
+                        v-for="link of links"
+                        :key="link.title"
+                        :to="link.url"
+                >
+                    <v-list-tile-action>
+                        <v-icon>{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="link.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
         <v-toolbar app dark color="primary">
-            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-side-icon
+                    @click="drawer = !drawer"
+                    class="hidden-md-and-up"
+            >
+            </v-toolbar-side-icon>
             <v-toolbar-title>VUESPA</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn flat>Link One</v-btn>
+                <v-btn
+                    v-for="link of links"
+                    :key="link.title"
+                    :to="link.url"
+                    flat>
+                    <v-icon left>{{ link.icon }}</v-icon>
+                    {{ link.title }}
+                </v-btn>
             </v-toolbar-items>
         </v-toolbar>
         <v-content>
@@ -15,5 +43,18 @@
     </v-app>
 </template>
 <script>
-    export default {}
+  export default {
+    data () {
+      return {
+        drawer: false,
+        links: [
+          {title: 'Login', icon: 'lock', url: '/login'},
+          {title: 'Registration', icon: 'face', url: '/registration'},
+          {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
+          {title: 'New ad', icon: 'note_add', url: '/new'},
+          {title: 'My ads', icon: 'list', url: '/list'}
+        ]
+      }
+    }
+  }
 </script>
