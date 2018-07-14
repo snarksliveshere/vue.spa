651d87a548b6ee9119c91f139e45fad236bf5b3a
snarksliveshere <snarksliveshere@gmail.com>
2018-07-14 14:48:28 +0300

    add all vue static pages

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,23 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-card-media
+                        src="https://dummyimage.com/1440x500/f2f2f2"
+                        height="300px"
+                    >
+
+                    </v-card-media>
+                    <v-cart-text>
+                        <h1 class="text--primary">Lorem</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, reprehenderit.</p>
+                    </v-cart-text>
+                    <v-cart-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" flat>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-cart-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,10 +1,34 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12 sm6 offet-sm3>
+            <v-flex xs12 sm6 offset-sm3>
                 <h1 class="text--secondary mb3">My ads</h1>
-                <v-card>
-
+                <v-card class="elevation-10 mb-3"
+                        v-for="ad in ads"
+                        :key="ad.id"
+                >
+                    <v-layout row>
+                        <v-flex xs4>
+                            <v-card-media
+                                    :src="ad.imageSrc"
+                                    height="160px"
+                            >
+                            </v-card-media>
+                        </v-flex>
+                        <v-flex xs8>
+                            <v-card-text>
+                                <h2 class="text--primary">{{ ad.title }}</h2>
+                                <p>{{ ad.description }}</p>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn class="info"
+                                       :to="'/ad/' + ad.id"
+                                >Open
+                                </v-btn>
+                            </v-card-actions>
+                        </v-flex>
+                    </v-layout>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -14,7 +38,22 @@
   export default {
     data () {
       return {
-
+        ads: [
+          {
+            title: 'first img',
+            description: 'first desc',
+            promo: false,
+            imageSrc: 'https://dummyimage.com/1440x500',
+            id: '123'
+          },
+          {
+            title: 'second img',
+            description: 'second desc',
+            promo: true,
+            imageSrc: 'https://dummyimage.com/1440x500/f2f2f2',
+            id: '1234'
+          }
+        ]
       }
     }
   }
