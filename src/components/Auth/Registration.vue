977bd657f02699ccc9bae6993d4f5756d993e4de
snarksliveshere <snarksliveshere@gmail.com>
2018-07-14 13:15:17 +0300

    add registration form

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,8 +1,50 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Registration</h1>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md6>
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="primary">
+                        <v-toolbar-title>Registration form</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form v-model="valid" ref="form" lazy-validation>
+                            <v-text-field
+                                    prepend-icon="person"
+                                    name="email"
+                                    label="emal"
+                                    type="email"
+                                    v-model="email"
+                                    :rules="emailRules"
+                            ></v-text-field>
+                            <v-text-field
+                                    prepend-icon="lock"
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    v-model="password"
+                                    :counter="6"
+                                    :rules="passwordRules"
+                            ></v-text-field>
+                            <v-text-field
+                                    prepend-icon="lock"
+                                    name="confirm-password"
+                                    label="Confirm password"
+                                    type="password"
+                                    v-model="confirmPassword"
+                                    :counter="6"
+                                    :rules="confirmPasswordRules"
+                            ></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                color="primary"
+                                @click="onSubmit"
+                                :disabled="!valid"
+                        >Create account</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +53,33 @@
   export default {
     data () {
       return {
-
+        email: '',
+        password: '',
+        confirmPassword: '',
+        valid: false,
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        passwordRules: [
+          v => !!v || 'Password is required',
+          v => v.length >= 6 || 'Password must be equal or more than 6 characters'
+        ],
+        confirmPasswordRules: [
+          v => !!v || 'Password is required',
+          v => v === this.password || 'Passwords should match'
+        ]
+      }
+    },
+    methods: {
+      onSubmit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            email: this.email,
+            password: this.password
+          }
+          console.log(user)
+        }
       }
     }
   }
