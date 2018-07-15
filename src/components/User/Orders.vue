013a27bfe8c015f2c1d864ecaf3008ad4d9dde3e
snarksliveshere <snarksliveshere@gmail.com>
2018-07-15 14:59:13 +0300

    add orders

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,7 +1,14 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
+            <v-flex xs12 class="text-xs-center pt-5" v-if="loading">
+                <v-progress-circular
+                        :size="100"
+                        color="primary"
+                        indeterminate
+                ></v-progress-circular>
+            </v-flex>
+            <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
                 <h1 class="text--secondary mb-3">Orders</h1>
                 <v-list
                         subheader
@@ -15,8 +22,8 @@
                         <v-list-tile-action>
                             <v-checkbox
                                     color="success"
-                                    @change="markDown(order)"
-                                    :input.value="order.done"
+                                    :input-value="order.done"
+                                    @change="markDone(order)"
                             ></v-checkbox>
                         </v-list-tile-action>
                         <v-list-tile-content>
@@ -32,28 +39,33 @@
                     </v-list-tile>
                 </v-list>
             </v-flex>
+            <v-flex xs12 class="text-xs-center" v-else>
+                <h1 class="text--secondary">U`ve no orders</h1>
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
 <script>
   export default {
-    data () {
-      return {
-        orders: [
-          {
-            id: 'fsd3',
-            name: 'Ste',
-            phone: '4546465654',
-            adId: '123',
-            done: false
-          }
-        ]
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      },
+      orders () {
+        return this.$store.getters.orders
       }
     },
     methods: {
       markDone (order) {
-        order.done = true
+        this.$store.dispatch('markOrderDone', order.id)
+          .then(() => {
+            order.done = true
+          })
+          .catch(() => {})
       }
+    },
+    created () {
+      this.$store.dispatch('fetchOrders')
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import BuyModalComponent from '@/components/Shared/BuyModal'
 import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
-
 Vue.use(Vuetify)
-
+Vue.component('app-buy-modal', BuyModalComponent)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
